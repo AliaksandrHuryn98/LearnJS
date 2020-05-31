6dbc44e9a040b23791ead6b6e9ae0cf5eeebc00d
aliaksandrhuryn <alexandergurin98@gmail.com>
2020-05-31 15:55:09 +0300

    Methods and properties

--- js/index.js
+++ js/index.js
@@ -150,3 +150,22 @@ console.log(str);
 const fruit = "Some fruit";
 
 console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+// console.log(logg.slice(6,11));
+
+
+// console.log(logg.slice(6));
+
+console.log(logg.substring(6,11));
+
+console.log(logg.substr(6,5)); //position and amount of symbols
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
